debcf863328d114215a32ae18df85fe293d0283d
frknpg <frknpg@gmail.com>
2023-12-08 23:44:52 +0300

    improvements on exception handler class

--- src/main/java/com/emlakjet/purchasing/exception/handler/PurchasingServiceExceptionHandler.java
+++ src/main/java/com/emlakjet/purchasing/exception/handler/PurchasingServiceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.emlakjet.purchasing.exception.handler;
 
 import com.emlakjet.purchasing.exception.AuthenticationException;
+import com.emlakjet.purchasing.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -21,41 +22,79 @@ public class PurchasingServiceExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(PurchasingServiceExceptionHandler.class);
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<String> handle(AccessDeniedException ex) {
+    public ResponseEntity<GenericResponse<String>> handle(AccessDeniedException ex) {
         LOG.error("Access denied exception!", ex);
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body("You are not authorized to perform this action.");
+                .body(GenericResponse.<String>builder()
+                        .code(HttpStatus.FORBIDDEN.value())
+                        .data(ex.getMessage())
+                        .message("Access denied!")
+                        .success(false)
+                        .build());
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> handle(AuthenticationException ex) {
+    public ResponseEntity<GenericResponse<String>> handle(AuthenticationException ex) {
         LOG.error("Authentication exception!", ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(ex.getMessage());
+                .body(GenericResponse.<String>builder()
+                        .code(HttpStatus.UNAUTHORIZED.value())
+                        .data(ex.getMessage())
+                        .message("Authentication failed!")
+                        .success(false)
+                        .build());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handle(IllegalArgumentException ex) {
+    public ResponseEntity<GenericResponse<String>> handle(IllegalArgumentException ex) {
         LOG.error("Illegal argument exception!", ex);
-        return ResponseEntity.badRequest().body("Illegal argument exception! " + ex.getMessage());
+        return ResponseEntity.badRequest().body(
+                GenericResponse.<String>builder()
+                        .code(HttpStatus.BAD_REQUEST.value())
+                        .data(ex.getMessage())
+                        .message("Illegal argument!")
+                        .success(false)
+                        .build()
+        );
     }
 
     @ResponseStatus(value = HttpStatus.CONFLICT)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> conflict(DataIntegrityViolationException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<GenericResponse<String>> conflict(DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                GenericResponse.<String>builder()
+                        .code(HttpStatus.CONFLICT.value())
+                        .data(e.getMessage())
+                        .message("Data integrity violation!")
+                        .success(false)
+                        .build()
+        );
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handle(RuntimeException ex) {
+    public ResponseEntity<GenericResponse<String>> handle(RuntimeException ex) {
         LOG.error("Service runtime exception!", ex);
-        return ResponseEntity.badRequest().body("Service runtime exception! " + ex.getMessage());
+        return ResponseEntity.badRequest().body(
+                GenericResponse.<String>builder()
+                        .code(HttpStatus.BAD_REQUEST.value())
+                        .data(ex.getMessage())
+                        .message("Service runtime exception!")
+                        .success(false)
+                        .build()
+        );
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handle(Exception ex) {
+    public ResponseEntity<GenericResponse<String>> handle(Exception ex) {
         LOG.error("Internal server error!", ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("A server occurred a error!");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                GenericResponse.<String>builder()
+                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .data(ex.getMessage())
+                        .message("Internal server error!")
+                        .success(false)
+                        .build()
+        );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
